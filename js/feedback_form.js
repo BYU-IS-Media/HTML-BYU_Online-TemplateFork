af2c1fbc97c2696f82a3ce35e58879a107086869
mkylemartin <mkylemartin@gmail.com>
2016-01-06 16:40:22 -0700

    feedback button

--- js/feedback_form.js
+++ js/feedback_form.js
@@ -9,6 +9,9 @@
 			url:"//indstudy1.org/qualtrics.php?form_id=SV_d5U4bk8YUwDOpFP",
 			success: function ( data, status, jqXHR ) {
 				console.log(status, data, jqXHR);
+				//$("#feedback-form").empty();
+				$("#feedback-form input").each(function( i,el) { $(el).val("");});
+				$("#feedback-form #submit-message").html('<div>Thank you for your feedback!</div>');
 			},
 			beforeSend: function ( jqXHR, settings ) {
 				var formData = $("#feedback-form").serialize();
@@ -19,8 +22,6 @@
 	}
 	
 	$(document).ready(function(){
-		$("#feedback-form button").on("click",submitFeedbackForm);
+		$("#feedback-form #submit").on("click",submitFeedbackForm);
 	});
-	
-	$( "#feedback-form" ).submit(submitFeedbackForm());
 })(jQuery);
